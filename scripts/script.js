a4b9234b05f4572aa1d092c2ad8ac0074c41c19d
Jair Murillo <brook_98@live.com>
2023-08-22 00:13:23 -0600

    haciendo correcion, no funciona en el deploy

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,20 @@
 const btnGetData = document.getElementById('btn-obtener');
 
 btnGetData.addEventListener('click', checkDataStorage);
-
-
-function checkDataStorage(){  //se checa el localstorage
-
-    const users = localStorage.getItem('users');  //obtenemos los valores con la palabra clave que se le coloco 'users'
-    const usersObject = JSON.parse(users);  //se pasa a objeto
-    const getTimeActual = usersObject[0].createdAt  //se obteiene el atributo createAt que es la hora en ms en que se creo 
-    const timeDiference = lifeSpan();
-    const differenceTime = timeDiference-getTimeActual
+    var users = localStorage.getItem('users');  //obtenemos los valores con la palabra clave que se le coloco 'users'
+    var usersObject = JSON.parse(users);  //se pasa a objeto
+    var timeDifference = lifeSpan();
+    var getTimeActual = usersObject[0].createdAt;  //se obteiene el atributo createAt que es la hora en ms en que se creo 
+    var differenceTime = timeDifference-getTimeActual;  
+     
+function checkDataStorage(){  //se checa el localstorage  
+    
 
 
     if( !users ) {  //si no esta 'users' en el local storage, se inicializa la solicitud 
 
         showUsers();
+        
 
     } else if ( differenceTime > 60000 ) {     //si la diferencia del tiempo en que se creo con la fecha actual es mayor a 1 min igual se inicializa la solitud   
         
